c35ec9e66288665e8cd2b37c1ada76adf8aa75b8
lewistorrington <lewis.torrington@upcloud.com>
2023-10-21 23:11:51 +0300

    feat: disgusting code, beautiful output

--- kasu.sh
+++ kasu.sh
@@ -21,30 +21,29 @@ echo "$MODE deps will be installed"
 
 # assuming you're root
 
-gum spin --spinner line --title "🛜 Turning swap off, setting ip-forwarding on..." -- apt update && apt upgrade -y;
-apt-get install -y apt-transport-https ca-certificates;
+gum spin --spinner line --title "🛜 Turning swap off, setting ip-forwarding on" -- apt update
+gum spin --spinner line --title "🛜 Turning swap off, setting ip-forwarding on" -- apt upgrade -y
+gum spin --spinner line --title "🛜 Turning swap off, setting ip-forwarding on" -- apt-get install -y apt-transport-https ca-certificates
 
-swapoff -a;
-{ head -n -1 /etc/fstab && tail -n 1 /etc/fstab | sed 's/^/#/'; } > /etc/fstab-temp && mv /etc/fstab-temp /etc/fstab;
+gum spin --spinner line --title "🛜 Turning swap off, setting ip-forwarding on" -- swapoff -a
+{ head -n -1 /etc/fstab && tail -n 1 /etc/fstab | sed 's/^/#/'; } > /etc/fstab-temp && mv /etc/fstab-temp /etc/fstab
 
-sysctl net.ipv4.ip_forward=1;
-sed -i '/#net.ipv4.ip_forward=1/s/^#//' /etc/sysctl.conf
+gum spin --spinner line --title "🛜 Turning swap off, setting ip-forwarding on" -- sysctl net.ipv4.ip_forward=1
+gum spin --spinner line --title "🛜 Turning swap off, setting ip-forwarding on" -- sed -i '/#net.ipv4.ip_forward=1/s/^#//' /etc/sysctl.conf
 
-# echo "🛜 Installed curl, turned swap off, set ip-forwarding on"
-
-gum spin --spinner line --title "🚢 Installing containerd-1.7.6 and runc-1.1.9" -- wget https://github.com/containerd/containerd/releases/download/v1.7.6/containerd-1.7.6-linux-amd64.tar.gz;
-tar Cxzvf /usr/local containerd-1.7.6-linux-amd64.tar.gz;
-wget https://github.com/opencontainers/runc/releases/download/v1.1.9/runc.amd64;
-install -m 755 runc.amd64 /usr/local/sbin/runc;
-wget https://github.com/containernetworking/plugins/releases/download/v1.3.0/cni-plugins-linux-amd64-v1.3.0.tgz;
-mkdir -p /opt/cni/bin;
-tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.3.0.tgz;
-mkdir /etc/containerd;
-containerd config default | tee /etc/containerd/config.toml;
-sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml;
-curl -L https://raw.githubusercontent.com/containerd/containerd/main/containerd.service -o /etc/systemd/system/containerd.service;
-systemctl daemon-reload;
-systemctl enable --now containerd;
+gum spin --spinner line --title "🚢 Installing containerd-1.7.6 and runc-1.1.9" -- wget https://github.com/containerd/containerd/releases/download/v1.7.6/containerd-1.7.6-linux-amd64.tar.gz
+gum spin --spinner line --title "🚢 Installing containerd-1.7.6 and runc-1.1.9" -- tar Cxzvf /usr/local containerd-1.7.6-linux-amd64.tar.gz
+gum spin --spinner line --title "🚢 Installing containerd-1.7.6 and runc-1.1.9" -- wget https://github.com/opencontainers/runc/releases/download/v1.1.9/runc.amd64
+gum spin --spinner line --title "🚢 Installing containerd-1.7.6 and runc-1.1.9" -- install -m 755 runc.amd64 /usr/local/sbin/runc
+gum spin --spinner line --title "🚢 Installing containerd-1.7.6 and runc-1.1.9" -- wget https://github.com/containernetworking/plugins/releases/download/v1.3.0/cni-plugins-linux-amd64-v1.3.0.tgz
+gum spin --spinner line --title "🚢 Installing containerd-1.7.6 and runc-1.1.9" -- mkdir -p /opt/cni/bin
+gum spin --spinner line --title "🚢 Installing containerd-1.7.6 and runc-1.1.9" -- tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.3.0.tgz
+gum spin --spinner line --title "🚢 Installing containerd-1.7.6 and runc-1.1.9" -- mkdir /etc/containerd
+containerd config default | tee /etc/containerd/config.toml
+gum spin --spinner line --title "🚢 Installing containerd-1.7.6 and runc-1.1.9" -- sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+gum spin --spinner line --title "🚢 Installing containerd-1.7.6 and runc-1.1.9" -- curl -L https://raw.githubusercontent.com/containerd/containerd/main/containerd.service -o /etc/systemd/system/containerd.service
+gum spin --spinner line --title "🚢 Installing containerd-1.7.6 and runc-1.1.9" -- systemctl daemon-reload
+gum spin --spinner line --title "🚢 Installing containerd-1.7.6 and runc-1.1.9" -- systemctl enable --now containerd
 echo "# these first two endpoint setting is where you configure crictl to containerd
 runtime-endpoint: unix:///run/containerd/containerd.sock
 image-endpoint: unix:///run/containerd/containerd.sock
@@ -52,46 +51,37 @@ timeout: 2
 debug: true
 pull-image-on-create: false" | sudo tee /etc/crictl.yaml
 
-# echo "🚢 Installed containerd-1.7.6 and runc-1.1.9"
+gum spin --spinner line --title "☸️ Installing kubelet kubeadm kubectl, version 1.28" -- apt-get install -y kubelet kubeadm kubectl
+gum spin --spinner line --title "☸️ Installing kubelet kubeadm kubectl, version 1.28" -- apt-mark hold kubelet kubeadm kubectl
 
-gum spin --spinner line --title "☸️ Installing kubelet kubeadm kubectl, version 1.28" -- apt-get install -y kubelet kubeadm kubectl;
-apt-mark hold kubelet kubeadm kubectl
+gum spin --spinner line --title "🐝 Installing cilium" -- modprobe br_netfilter
+gum spin --spinner line --title "🐝 Installing cilium" -- echo "br_netfilter" | sudo tee /etc/modules-load.d/k8s.conf
+gum spin --spinner line --title "🐝 Installing cilium" -- systemctl restart systemd-modules-load
 
-# echo "☸️ Installed kubelet kubeadm kubectl, version 1.28"
-
-gum spin --spinner line --title "👷🐝 Installing helm and cilium" -- modprobe br_netfilter;
-echo "br_netfilter" | sudo tee /etc/modules-load.d/k8s.conf;
-systemctl restart systemd-modules-load;
-
-wget https://get.helm.sh/helm-v3.13.0-linux-amd64.tar.gz;
-tar -zxvf helm-v3.13.0-linux-amd64.tar.gz;
-mv linux-amd64/helm /usr/local/bin/helm;
-helm repo add cilium https://helm.cilium.io/
-
-# echo "👷🐝 Installed helm and cilium"
+gum spin --spinner line --title "🐝 Installing cilium" -- wget https://get.helm.sh/helm-v3.13.0-linux-amd64.tar.gz
+gum spin --spinner line --title "🐝 Installing cilium" -- tar -zxvf helm-v3.13.0-linux-amd64.tar.gz
+gum spin --spinner line --title "🐝 Installing cilium" -- mv linux-amd64/helm /usr/local/bin/helm
+gum spin --spinner line --title "🐝 Installing cilium" -- helm repo add cilium https://helm.cilium.io/
 
 gum spin --spinner line --title "📦 Pulling k8s images" -- kubeadm config images pull
 
-# echo "📦 Pulled k8s images"
-
 if [ $MODE == "control-plane" ]; then
     echo "⚙️ Initialising the control plane"
-    kubeadm init --skip-phases=addon/kube-proxy
-    gum spin --spinner line --title "✨ Final touches: KUBECONFIG=/etc/kubernetes/admin.conf, activating cilium" -- mkdir -p $HOME/.kube &&
-    cp -i /etc/kubernetes/admin.conf $HOME/.kube/config &&
-    chown $(id -u):$(id -g) $HOME/.kube/config &&
-    export KUBECONFIG=/etc/kubernetes/admin.conf &&
 
-    # helm install cilium cilium/cilium --version 1.14.2 --namespace kube-system;
-    curl -LO https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz &&
-    sudo tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin &&
-    rm cilium-linux-amd64.tar.gz &&
-    cilium install
+    kubeadm init --skip-phases=addon/kube-proxy # TODO: allow possible VPN IP(s) in front for the TLS cert
+
+    gum spin --spinner line --title "✨ Final touches: KUBECONFIG=/etc/kubernetes/admin.conf, activating cilium" -- mkdir -p $HOME/.kube
+    gum spin --spinner line --title "✨ Final touches: KUBECONFIG=/etc/kubernetes/admin.conf, activating cilium" -- cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    gum spin --spinner line --title "✨ Final touches: KUBECONFIG=/etc/kubernetes/admin.conf, activating cilium" -- chown $(id -u):$(id -g) $HOME/.kube/config
+    gum spin --spinner line --title "✨ Final touches: KUBECONFIG=/etc/kubernetes/admin.conf, activating cilium" -- export KUBECONFIG=/etc/kubernetes/admin.conf
+
+    gum spin --spinner line --title "✨ Final touches: KUBECONFIG=/etc/kubernetes/admin.conf, activating cilium" -- curl -LO https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz
+    gum spin --spinner line --title "✨ Final touches: KUBECONFIG=/etc/kubernetes/admin.conf, activating cilium" -- sudo tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin
+    gum spin --spinner line --title "✨ Final touches: KUBECONFIG=/etc/kubernetes/admin.conf, activating cilium" -- rm cilium-linux-amd64.tar.gz
+    gum spin --spinner line --title "✨ Final touches: KUBECONFIG=/etc/kubernetes/admin.conf, activating cilium" -- cilium install
 
     echo "✅ Done! Check it yourself with crictl ps"
 elif [ $MODE == "worker" ]; then
     JOIN_COMMAND=$(gum input --width 500 --placeholder "copy-paste your 'kubeadm join YOUR_TOKENS_HERE' (as 1 line!!!)")
     eval "$JOIN_COMMAND"
-    sleep 3
-    systemctl restart containerd.service
 fi
